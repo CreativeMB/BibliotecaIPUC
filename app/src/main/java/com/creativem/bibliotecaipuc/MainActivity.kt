fa8ad58662fca387f481be671ec2f4147086c88b
tobflowerr <ingcpu@gmail.com>
2023-06-12 21:07:28 -0500

    la prime ventana

---
+++ app/src/main/java/com/creativem/bibliotecaipuc/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.creativem.bibliotecaipuc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
